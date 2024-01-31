0244e1b838b10a34b5f108ba5e017424dcfe3558
Engin EFE <elektroengin@gmail.com>
2024-01-31 20:21:25 +0300

    abstract ödev

---
+++ TasarimDesenleri/abstractV3/Musteri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abstractV3
+{
+    class Musteri
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
